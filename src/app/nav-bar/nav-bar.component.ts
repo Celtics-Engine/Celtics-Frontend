e13153171e4494b69a46af220ec1c92803578777
Kyle Kovalchick <Kovalchickzz@gmail.com>
2022-02-13 19:19:47 -0600

    Started work on frontend

---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss']
+})
+export class NavBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onUserLogin(): void{
+    console.log("Login")
+  }
+
+}
