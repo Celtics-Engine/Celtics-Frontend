59a262891e93dd9317e2f7667f286d2191c39f56
Will Reynolds <billiamreynolds@gmail.com>
2022-02-16 15:53:15 -0600

    actually clean project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BucketComponent } from './bucket/bucket.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    BucketComponent
   ],
   imports: [
     BrowserModule
-  ],
+  ]
   providers: [],
   bootstrap: [AppComponent]
 })
