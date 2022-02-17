c1437b53e03900c85c20faa5308c4c1e68878e23
Kyle Kovalchick <Kovalchickzz@gmail.com>
2022-02-16 22:13:02 -0600

    Pogging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import Amplify, {Auth} from "aws-amplify";
+import awsConfig from "../aws-exports";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Celtics-Frontend';
+
+  constructor() {
+    Amplify.configure(awsConfig);
+    Auth.configure(awsConfig);
+  }
+
 }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -49,9 +49,8 @@ import 'zone.js';  // Included with Angular CLI.
 
 
 (window as any).global = window;
-global.Buffer = global.Buffer || require('buffer').Buffer;
 (window as any).process = {
-  version: ''
+  env: { DEBUG: undefined },
 };
 
 /***************************************************************************************************
