baa22b4524b35df1604e0047cfe642c7b644e26c
Will Reynolds <billiamreynolds@gmail.com>
2022-02-21 17:47:29 -0600

    auth component is now just called "app-login"

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -47,7 +47,7 @@
  */
 import 'zone.js';  // Included with Angular CLI.
 
-
+(window as any).Zone['__zone_symbol__ignoreConsoleErrorUncaughtError'] = true;
 (window as any).global = window;
 (window as any).process = {
   env: { DEBUG: undefined },
