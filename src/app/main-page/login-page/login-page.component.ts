38392929b18362d95697e4d656f3781d2589325e
Will Reynolds <billiamreynolds@gmail.com>
2022-02-21 19:59:13 -0600

    Re-added page state changes to login state

--- src/app/main-page/login-page/login-page.component.ts
+++ src/app/main-page/login-page/login-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {WebsiteStateService} from "../../services/website-state/website-state.service";
+import {Auth} from 'aws-amplify';
+
 
 @Component({
   selector: 'app-login-page',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
+  loggedIn = false;
 
-  constructor() { }
+  user: Promise<any> = new Promise<any>(resolve => {
+    Auth.currentAuthenticatedUser()
+      .then(user => {
+        resolve(user);
+        this.loggedIn = true;
+        console.log(user);
+      })
+      .catch(err => {
+        resolve(null);
+        this.loggedIn = false;
+        console.log(err);
+      });
+  });
+  userName = this.getUserName()
+
+
+  constructor(private websiteState: WebsiteStateService) {
+    websiteState.loggedIn$.subscribe(state => {
+      this.loggedIn = state;
+    })
+    websiteState.username$.subscribe(state => {
+      this.userName = state;
+    })
+  }
 
   ngOnInit(): void {
+
+  }
+
+  getUserName(): string {
+    this.user.then(user => {
+      this.userName = user.username;
+    })
+    return this.userName;
   }
 
 }
