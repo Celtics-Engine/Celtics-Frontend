d765275aa6e76cb3ca964baad24bd8ebcfe71178
Will Reynolds <billiamreynolds@gmail.com>
2022-02-25 18:27:32 -0600

    better icons that actually make sense

--- src/app/nav-bar/menu-options.ts
+++ src/app/nav-bar/menu-options.ts
@@ -1,9 +1,9 @@
 import {Option} from './Option';
 import {PageState} from "../types/page-state";
 import {faCaretSquareDown, faCaretSquareUp} from '@fortawesome/free-solid-svg-icons';
-import { faCalculator } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { faChartLine } from '@fortawesome/free-solid-svg-icons';
-import { faChartPie } from '@fortawesome/free-solid-svg-icons';
+import { faFileExport } from '@fortawesome/free-solid-svg-icons';
 
 
 
@@ -16,17 +16,17 @@ export const MENU_OPTIONS: Option[] = [
   {
     state: PageState.PROFILE,
     label: 'Profile',
-    icon: faCalculator
+    icon: faUser
   },
   {
     state: PageState.SEARCH,
-    label: 'Search',
+    label: 'Explore',
     icon: faChartLine
   },
   {
     state: PageState.ASSET_POST,
     label: 'Asset Post',
-    icon: faChartPie
+    icon: faFileExport
   },
   {
     state: PageState.LOGOUT,
